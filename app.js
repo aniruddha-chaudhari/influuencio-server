77492c951618b8dbaf09ef9e89b7473bbf50009a
aniruddha-chaudhari <140920431+aniruddha-chaudhari@users.noreply.github.com>
2024-06-25 02:16:56 +0530

    test

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 import express from 'express';
 import cors from 'cors';
 import pool from './src/db/db.js';
+import { google } from 'googleapis';
+import { config } from 'dotenv';
+
+config();
+
+const youtube = google.youtube({
+  version: 'v3',
+  auth: process.env.YOUTUBE_API_KEY
+})
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.get('/users', async (req, res) => {
+app.get('/:channelname', async (req, res) => {
+  const channelname = req.params.channelname;
   try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
+    const searchResponse = await new Promise((resolve, reject) => {
+      youtube.search.list({
+        part: 'id',
+        type: 'channel',
+        q: channelname,
+        maxResults: 1
+      }, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+
+  const channelId = searchResponse.data.items[0].id.channelId;
+  res.json(channelId);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Database query failed' });
   }
-});
+
+})
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
